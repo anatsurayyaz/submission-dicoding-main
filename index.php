32837c1d475c8815b7e6554de0bb93cda4092246
Ana Tsurayya Zakiya <anatsurayya03@gmail.com>
2023-06-01 02:51:56 +0700

    Add project files.

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Profil Saya</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+      background-color: lavenderblush;
+    }
+
+    .container {
+      max-width: 600px;
+      margin: 0 auto;
+      padding: 40px;
+      background-color: #fff;
+      border-radius: 8px;
+      box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+    }
+
+    .profile-image {
+      width: 200px;
+      height: 200px;
+      border-radius: 50%;
+      margin-bottom: 20px;
+      object-fit: cover;
+      object-position: center;
+      box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+    }
+
+    h1 {
+      color: #333;
+      margin-bottom: 20px;
+      text-align: center;
+    }
+
+    h2 {
+      color: #666;
+      margin-bottom: 10px;
+    }
+
+    p {
+      color: #333;
+      line-height: 1.5;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <h1>Profil Saya</h1>
+    <img class="profile-image" src="dhymas.JPEG" alt="Foto Profil">
+
+    <h2>Nama:</h2>
+    <p>Dhymas Bagus Tri Pamungkas</p>
+
+    <h2>Alamat:</h2>
+    <p>Kulonprogo, Yogyakarta</p>
+
+    <h2>Deskripsi:</h2>
+    <p>saya adalah seorang mahasiswa semester 6 Program Studi Informatika di UIN Sunan Kalijaga Yogyakarta</p>
+  </div>
+</body>
+</html>
